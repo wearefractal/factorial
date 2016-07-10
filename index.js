bbf1f1afa35b78e0d78b2fabc18f4da2890add2e
LudwikJaniuk <ludvig.janiuk@gmail.com>
2016-07-10 23:13:51 +0200

    Made it throw if given negative number
    Instead of going into an infinite recursion, you know

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-module.exports = factorial;
+module.exports = function(n) {
+  if (n < 0) {
+    throw new RangeError("Cannot take factorial out of a negative number.");
+  } else {
+    return factorial(n);
+  }
+}
 
-function factorial(v) {
-  return v === 0 ? 1 : v * factorial(v - 1);
+function factorial(n) {
+  return n === 0 ? 1 : n * factorial(n - 1);
 }
