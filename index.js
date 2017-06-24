b48106481975104ebfb693ef4055d8d1aef05831
contra <contra@users.noreply.github.com>
2017-06-24 14:17:12 -0400

    Revert "optimize"

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-module.exports = function factorial(n) {
-  if (n === 0) return 1
-  if (n < 0) return NaN
-  for (var r = 1; n > 1; n--) r *= n
-  return r
+module.exports = function(n) {
+  if (n < 0) {
+    throw new RangeError("Cannot take factorial out of a negative number.");
+  } else {
+    return factorial(n);
+  }
+}
+
+function factorial(n) {
+  return n === 0 ? 1 : n * factorial(n - 1);
 }
