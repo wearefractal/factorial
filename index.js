638456f9be5ab76220494e5fcbd0811ad9945eb7
Joey Paris <joey@leadjig.com>
2021-06-21 15:36:18 -0400

    Add support for negative starting integers
    Includes testing over every number from -99 to +99
    Uses BigInt to work with extremely large numbers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = function factorial(n) {
-  if (n === 0) return 1
-  if (n < 0) return NaN
-  for (var r = 1; n > 1; n--) r *= n
-  return r
+  var sign = BigInt(Math.sign(n))
+  n = BigInt(Math.abs(n))
+  if (n == 0) return 1
+  for (var r = BigInt(1); n > 1; n--) r *= n
+  return r * sign
 }
--- test/main.js
+++ test/main.js
@@ -1,22 +1,13 @@
 var factorial = require('../');
 var should = require('should');
+var test_data = require('./test_data')
 require('mocha');
 
 describe('factorial', function() {
-  it('should return NaN with -1', function(done) {
-    isNaN(factorial(-1)).should.equal(true);
-    done();
-  });
-  it('should work with 0', function(done) {
-    factorial(0).should.equal(1);
-    done();
-  });
-  it('should work with 10', function(done) {
-    factorial(10).should.equal(3628800);
-    done();
-  });
-  it('should work with 21', function(done) {
-    factorial(21).should.equal(51090942171709440000);
-    done();
-  });
+  Object.keys(test_data).forEach((number) => {
+    it(`should work with ${number}`, function(done) {
+      factorial(number).should.equal(test_data[number])
+      done();
+    })
+  })
 });
